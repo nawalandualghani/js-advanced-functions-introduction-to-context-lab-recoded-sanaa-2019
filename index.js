bdb2946bcc496d3aa4960f2ac2d963f8ece5ca8e
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-26 13:16:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function createEmployeeRecord(arr){
+let employeeRecord={
+  firstName: arr[0],
+       familyName: arr[1],
+       title: arr[2],
+       payPerHour: arr[3],
+       timeInEvents: [],
+       timeOutEvents: []
+}
+return employeeRecord;
+}
